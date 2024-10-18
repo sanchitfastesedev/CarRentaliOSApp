e14cb1c1989a181e1b8d5243b48cb666b693cb4a
sanchitmehta <sanchitmehta@microsoft.com>
2024-10-18 19:54:17 +0530

    feat: added padding utility

---
+++ CarRentalApp/Utility.swift
@@ -0,0 +1,23 @@
+//
+//  Utility.swift
+//  CarRentalApp
+//
+//  Created by Sanchit Mehta on 18/10/24.
+//
+
+import Foundation
+
+extension CGFloat {
+    static let p2: CGFloat = 2.0
+    static let p4: CGFloat = 4.0
+    static let p6: CGFloat = 6.0
+    static let p8: CGFloat = 8.0
+    static let p10: CGFloat = 10.0
+    static let p16: CGFloat = 16.0
+    static let p20: CGFloat = 20.0
+    static let p26: CGFloat = 26.0
+    static let p32: CGFloat = 32.0
+    static let p18: CGFloat = 18.0
+}
+
+
