24c19d09ca9259dd237f577f49b57122cd857ac3
sanchitmehta <sanchitmehta@microsoft.com>
2024-10-18 15:47:23 +0530

    Initial Commit

---
+++ CarBookingApp/CarBookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarBookingAppApp.swift
+//  CarBookingApp
+//
+//  Created by Apple on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CarBookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
